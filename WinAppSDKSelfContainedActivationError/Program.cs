8a3c033d6629ca6562783126d94899edc763a6a1
Justin Soderstrom <jsoderstrom@cccis.com>
2023-02-15 07:29:05 -0600

    Add project files.

---
+++ WinAppSDKSelfContainedActivationError/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.UI.Dispatching;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using Microsoft.Windows.AppLifecycle;
+
+namespace WinAppSDKSelfContainedActivationError;
+
+//https://learn.microsoft.com/en-us/windows/apps/windows-app-sdk/migrate-to-windows-app-sdk/guides/applifecycle#instructions-for-c
+//https://github.com/microsoft/WindowsAppSDK-Samples/blob/main/Samples/AppLifecycle/Instancing/cs-winui-packaged/CsWinUiDesktopInstancing/CsWinUiDesktopInstancing/Program.cs
+class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+
+        bool isRedirect = DecideRedirection();
+        if (!isRedirect)
+        {
+            Microsoft.UI.Xaml.Application.Start((p) =>
+            {
+                var context = new DispatcherQueueSynchronizationContext(
+                    DispatcherQueue.GetForCurrentThread());
+                SynchronizationContext.SetSynchronizationContext(context);
+                new App();
+            });
+        }
+    }
+
+    private static bool DecideRedirection()
+    {
+        bool isRedirect = false;
+        AppActivationArguments args = AppInstance.GetCurrent().GetActivatedEventArgs();
+        ExtendedActivationKind kind = args.Kind;
+        AppInstance keyInstance = AppInstance.FindOrRegisterForKey("randomKey");
+
+        if (keyInstance.IsCurrent)
+        {
+            keyInstance.Activated += OnActivated;
+        }
+        else
+        {
+            isRedirect = true;
+            RedirectActivationTo(args, keyInstance);
+        }
+        return isRedirect;
+    }
+
+    private static void OnActivated(object sender, AppActivationArguments args)
+    {
+        ExtendedActivationKind kind = args.Kind;
+    }
+
+    // Do the redirection on another thread, and use a non-blocking
+    // wait method to wait for the redirection to complete.
+    public static void RedirectActivationTo(
+        AppActivationArguments args, AppInstance keyInstance)
+    {
+        var redirectSemaphore = new Semaphore(0, 1);
+        Task.Run(() =>
+        {
+            keyInstance.RedirectActivationToAsync(args).AsTask().Wait();
+            redirectSemaphore.Release();
+        });
+        redirectSemaphore.WaitOne();
+    }
+
+}
